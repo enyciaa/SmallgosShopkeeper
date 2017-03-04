4b28394e37b711ab974f3e0cba667a1d5d06b65b
enyciaa <ian.andrew.alexander@gmail.com>
2017-03-04 15:08:59 +0000

    Add views for inventory and orders

---
+++ app/src/main/java/smallgos/intuithack/com/smallgos/InventoryView.java
@@ -0,0 +1,29 @@
+package smallgos.intuithack.com.smallgos;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class InventoryView extends LinearLayout {
+
+    public InventoryView(Context context) {
+        super(context);
+    }
+
+    public InventoryView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public InventoryView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public InventoryView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+}
--- app/src/main/java/smallgos/intuithack/com/smallgos/MainActivity.java
+++ app/src/main/java/smallgos/intuithack/com/smallgos/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import smallgos.intuithack.com.smallgos.databinding.ActivityMainBinding;
@@ -12,27 +14,40 @@ import smallgos.intuithack.com.smallgos.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    InventoryView inventoryView;
+    OrderView orderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        LayoutInflater inflater = getLayoutInflater();
+        inventoryView = (InventoryView) inflater.inflate(R.layout.view_inventory, binding.mainContainer, false);
+        orderView = (OrderView) inflater.inflate(R.layout.view_orders, binding.mainContainer, false);
+
         binding.bottomNavigation.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
-                            case R.id.tabItems:
+                            case R.id.tabInventory:
+                                binding.mainContainer.removeAllViews();
+                                binding.mainContainer.addView(inventoryView);
                                 break;
 
                             case R.id.tabOrders:
+                                binding.mainContainer.removeAllViews();
+                                binding.mainContainer.addView(orderView);
                                 break;
 
                         }
                         return true;
                     }
                 });
+
+        Menu bottomNavigationMenu = binding.bottomNavigation.getMenu();
+        bottomNavigationMenu.performIdentifierAction(R.id.tabInventory, 0);
     }
 
 }
