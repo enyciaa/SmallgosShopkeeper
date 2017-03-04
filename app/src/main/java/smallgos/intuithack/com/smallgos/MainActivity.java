257b701925927d89985e0019e0cf0c467184a0b9
enyciaa <ian.andrew.alexander@gmail.com>
2017-03-04 23:05:24 +0000

    Removed bottom bar

--- app/src/main/java/smallgos/intuithack/com/smallgos/MainActivity.java
+++ app/src/main/java/smallgos/intuithack/com/smallgos/MainActivity.java
@@ -2,12 +2,8 @@ package smallgos.intuithack.com.smallgos;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import smallgos.intuithack.com.smallgos.databinding.ActivityMainBinding;
 
@@ -15,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     InventoryView inventoryView;
-    OrderView orderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         LayoutInflater inflater = getLayoutInflater();
         inventoryView = (InventoryView) inflater.inflate(R.layout.view_inventory, binding.mainContainer, false);
-        orderView = (OrderView) inflater.inflate(R.layout.view_orders, binding.mainContainer, false);
-
-        binding.bottomNavigation.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.tabInventory:
-                                binding.mainContainer.removeAllViews();
-                                binding.mainContainer.addView(inventoryView);
-                                break;
-
-                            case R.id.tabOrders:
-                                binding.mainContainer.removeAllViews();
-                                binding.mainContainer.addView(orderView);
-                                break;
-
-                        }
-                        return true;
-                    }
-                });
-
-        Menu bottomNavigationMenu = binding.bottomNavigation.getMenu();
-        bottomNavigationMenu.performIdentifierAction(R.id.tabInventory, 0);
+        binding.mainContainer.addView(inventoryView);
     }
 
 }
