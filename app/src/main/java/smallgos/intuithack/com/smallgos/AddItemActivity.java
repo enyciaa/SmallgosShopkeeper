4690ec2712c4a863e9c73f9db2452183592a59f5
enyciaa <ian.andrew.alexander@gmail.com>
2017-03-04 22:02:16 +0000

    Add all item info

--- app/src/main/java/smallgos/intuithack/com/smallgos/AddItemActivity.java
+++ app/src/main/java/smallgos/intuithack/com/smallgos/AddItemActivity.java
@@ -4,7 +4,9 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +21,6 @@ import smallgos.intuithack.com.smallgos.model.InventoryItem;
 public class AddItemActivity extends AppCompatActivity {
 
     private ActivityAddItemBinding binding;
-    private InventoryItem item = new InventoryItem();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +31,27 @@ public class AddItemActivity extends AppCompatActivity {
 
     @OnClick(R.id.itemAddButton)
     void addItem() {
+        InventoryItem item = new InventoryItem();
+        String name = binding.itemName.getText().toString();
+        String productId = binding.itemProductId.getText().toString();
+        String price = binding.itemPrice.getText().toString();
+        String description = binding.itemDescription.getText().toString();
+        String quantity = binding.itemQuantity.getText().toString();
+        if (!TextUtils.isEmpty(name)
+                && !TextUtils.isEmpty(productId)
+                && !TextUtils.isEmpty(price)
+                && !TextUtils.isEmpty(description)
+                && !TextUtils.isEmpty(quantity)) {
+            item.setName(name);
+            item.setProductId(productId);
+            item.setPrice(price);
+            item.setDescription(description);
+            item.setStock(quantity);
+        } else {
+            Toast.makeText(this, getString(R.string.data_missing), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         List<InventoryItem> items = new ArrayList<>();
         items.add(item);
 
